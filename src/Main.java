609e5e9ff5f9e64927176f4a0343e30bc23512eb
Nayaka <19623116@mahasiswa.itb.ac.id>
2025-02-24 11:01:06 +0700

    fix: minor changes

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Masukkan nama file .txt (tanpa ekstensi): ");
         String filename = scanner.nextLine();
+
+        // Parsing txt
         Data data = ReadTXT.readTxt("test/"+ filename + ".txt");
         
+        // Validasi (validate) input
         if (data == null) {
             System.out.println("Gagal membaca file T_T.");
             scanner.close();
             return;
         }
         
+        // Prep
         Papan papan = new Papan(data.getN(), data.getM());
         HashMap<Character, Keping> kepings = data.getKepings();
 
+        // Solve
         Solver solver = new Solver(papan, kepings);
         String hasil = solver.solve();
 
+        // Output
         System.out.println("Mau menyimpan hasil ke file? (y/n)");
         String save = scanner.nextLine();
         if (save.equalsIgnoreCase("y")) {
