1f3816ade655ecd10bb15942339aec59677a5c03
Nayaka <19623116@mahasiswa.itb.ac.id>
2025-02-24 12:01:13 +0700

    feat: add function for txt output

--- src/utils/WriteTXT.java
+++ src/utils/WriteTXT.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 public class WriteTXT {
     public static void writeTxt(String filename, String content) {
         try {
-            FileWriter fileWriter = new FileWriter("test/output_" + filename + ".txt");
+            FileWriter fileWriter = new FileWriter("test/outputs/output_" + filename + ".txt");
             fileWriter.write(content);
             fileWriter.close();
             System.out.println("Berhasil menyimpan file, Cik! :D");
-            System.out.println("Hasil disimpan di test/output_" + filename + ".txt");
+            System.out.println("Hasil disimpan di test/outputs/output_" + filename + ".txt");
         } catch (IOException e) {
             System.err.println("Gagal menyimpan file T_T.");
         }
